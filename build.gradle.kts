33f211d0ff871d87f14d77c57f981fa68b1da095
guoqiangzhengcn-code <guoqiangzhengcn@gmail.com>
2026-04-09 01:11:23 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ subprojects {
             freeCompilerArgs = listOf("-Xjvm-default=all", "-Xextended-compiler-checks")
         }
     }
-    configure<JavaPluginConvention> {
+    configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
