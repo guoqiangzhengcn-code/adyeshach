5a294fbd2aaaa29fabb1aa82089efced1bd384b6
guoqiangzhengcn-code <guoqiangzhengcn@gmail.com>
2026-04-09 18:21:38 +0800

    Update DefaultMinecraftHelper.kt

--- project/common-impl-nms/src/main/kotlin/ink/ptms/adyeshach/impl/nms/DefaultMinecraftHelper.kt
+++ project/common-impl-nms/src/main/kotlin/ink/ptms/adyeshach/impl/nms/DefaultMinecraftHelper.kt
@@ -222,7 +222,7 @@ class DefaultMinecraftHelper : MinecraftHelper {
             false
         }
     }
-        {
+        
     override fun toMinecraft(entity: Entity): Any {
         entity as CraftEntity9
         return entity.handle
